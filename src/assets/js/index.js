9a1d080c6af38b91cd7f9a57d2c62cea92205fa1
Justin Carlson <jrcarlson8@gmail.com>
2020-11-12 21:46:07 -0500

    Import fontawesome CSS instead of injecting.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,12 +1,17 @@
-import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import * as fontawesome from '@fortawesome/fontawesome-svg-core';
 import { faArtstation, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 import '../css/index.css';
 import '../images/favicon.svg'
 
-library.add(faArtstation, faGithub, faLinkedin);
-library.add(faEnvelope);
-dom.watch();
+// Don't inject CSS, we include it.
+fontawesome.config.autoAddCss = false;
 
+// Only add icons we use to reduce bundle size.
+fontawesome.library.add(faArtstation, faGithub, faLinkedin);
+fontawesome.library.add(faEnvelope);
+fontawesome.dom.watch();
+
+// Easter egg :^)
 console.log('%c>>> Taking a look under the hood? Check out the source code: https://github.com/justinbot/justinbot.github.io', 'background: #2aa198; color: #000000');
