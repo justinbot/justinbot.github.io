23f8b26675fb1b83986302370d9bb7ee9075498e
Justin Carlson <justin@justincarlson.io>
2021-03-10 22:52:01 -0500

    Update dependencies.

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = (config) => {
   config.setTemplateFormats(['njk']);
 
   // Support YAML data
-  config.addDataExtension('yaml', (contents) => yaml.safeLoad(contents));
+  config.addDataExtension('yaml', (contents) => yaml.load(contents));
 
   config.addPassthroughCopy('./src/build/');
   config.addPassthroughCopy({ './src/images/': 'images' });
